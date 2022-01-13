449fde0c7071edb8c5fb05dda7a64df77a275f42
Ian Holmes <concord511@gmail.com>
2022-01-13 17:16:35 -0600

    added more mutations and authentication with JWTs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -27,12 +27,26 @@ const typeDefs = gql`
         thoughts: [Thought]
         friends: [User]
     }
-    
+
     type Query {
+        me: User
         users: [User]
         user(username: String!): User
         thoughts(username: String): [Thought]
         thought(_id: ID!): Thought
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addThought(thoughtText: String!): Thought
+        addReaction(thoughtId: ID!, reactionBody: String!): Thought
+        addFriend(friendId: ID!): User
+    }
+
+    type Auth {
+        token: ID!
+        user: User
     }
 `;
 
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { authMiddleware } = require('./utils/auth');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 
@@ -14,7 +15,7 @@ const startServer = async () => {
   const server = new ApolloServer({ 
     typeDefs, 
     resolvers, 
-    // context: authMiddleware 
+    context: authMiddleware
   });
 
   // Start the Apollo server
